9f1c91e5aeabccc346eef97bef301173aee2d8e2
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2021-12-04 16:25:06 +1300

    run prettier

--- src/sidebar/App.tsx
+++ src/sidebar/App.tsx
@@ -2,9 +2,5 @@
 import { h } from 'preact'
 
 export const App = () => {
-  return (
-    <div data-testid='text'>
-      Hello World script
-    </div>
-  )
+    return <div data-testid="text">Hello World script</div>
 }
--- src/testing/setup/jsdom.js
+++ src/testing/setup/jsdom.js
@@ -1,12 +1,11 @@
-
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { JSDOM } = require('jsdom')
 
 const dom = new JSDOM('<body></body>', {
-  beforeParse (window) {
-    window.innerWidth = 200
-    window.innerHeight = 400
-  }
+    beforeParse(window) {
+        window.innerWidth = 200
+        window.innerHeight = 400
+    },
 })
 
 global.window = dom.window
