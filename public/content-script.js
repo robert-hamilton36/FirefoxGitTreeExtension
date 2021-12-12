0e629ddb61e86f075bdd231f85147bc1da7978e5
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2021-12-13 12:53:59 +1300

    add sendMessage on user events

--- public/content-script.js
+++ public/content-script.js
@@ -5,3 +5,18 @@ const getColour = () => {
 
 const colour = getColour()
 document.body.style.border = `5px solid ${colour}`
+
+browser.runtime.sendMessage({webpage: window.location.href})
+
+let url = window.location.href;
+
+['click','popstate'].forEach( evt =>
+  window.addEventListener(evt, function () {
+      requestAnimationFrame(()=>{
+        if (url !== location.href) {
+          browser.runtime.sendMessage({webpage: window.location.href})
+        }
+        url = location.href;
+      })
+  }, true)
+)
